43cce3a3f77e288fad7ac3afc3678f5041832436
takka <m@iladdr.es>
2017-08-08 16:24:11 +0900

    Initial Commit

---
+++ Domain/Domain.h
@@ -0,0 +1,19 @@
+//
+//  Domain.h
+//  Domain
+//
+//  Created by takka on 2017/08/08.
+//  Copyright © 2017年 takka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Domain.
+FOUNDATION_EXPORT double DomainVersionNumber;
+
+//! Project version string for Domain.
+FOUNDATION_EXPORT const unsigned char DomainVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Domain/PublicHeader.h>
+
+
---
+++ Presentation/Presentation.h
@@ -0,0 +1,19 @@
+//
+//  Presentation.h
+//  Presentation
+//
+//  Created by takka on 2017/08/08.
+//  Copyright © 2017年 takka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Presentation.
+FOUNDATION_EXPORT double PresentationVersionNumber;
+
+//! Project version string for Presentation.
+FOUNDATION_EXPORT const unsigned char PresentationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Presentation/PublicHeader.h>
+
+
