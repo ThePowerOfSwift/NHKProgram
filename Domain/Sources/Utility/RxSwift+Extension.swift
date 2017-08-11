b31e7f32e9ac097421d6287f275dfc1bf1cf440c
takka <m@iladdr.es>
2017-08-11 13:48:25 +0900

    OptionalのObservable, Driverからnilをフィルタするメソッドの追加

--- Domain/Sources/Utility/RxSwift+Extension.swift
+++ Domain/Sources/Utility/RxSwift+Extension.swift
@@ -8,7 +8,37 @@ public extension ObservableType where E == Bool {
     public func not() -> Observable<Bool> {
         return self.map(!)
     }
-    
+}
+
+public protocol OptionalType {
+    associatedtype Wrapped
+    var value: Wrapped? { get }
+}
+
+extension Optional: OptionalType {
+    public var value: Wrapped? { return self }
+}
+
+public extension ObservableType where E: OptionalType {
+    public func filterNil() -> Observable<E.Wrapped> {
+        return self.asObservable().flatMap { e -> Observable<E.Wrapped> in
+            guard let value = e.value else {
+                return Observable.empty()
+            }
+            return Observable.just(value)
+        }
+    }
+}
+
+public extension SharedSequenceConvertibleType where SharingStrategy == DriverSharingStrategy, E: OptionalType {
+    public func filterNil() -> Driver<E.Wrapped> {
+        return self.flatMap { e -> Driver<E.Wrapped> in
+            guard let value = e.value else {
+                return Driver.empty()
+            }
+            return Driver.just(value)
+        }
+    }
 }
 
 public extension SharedSequenceConvertibleType {
