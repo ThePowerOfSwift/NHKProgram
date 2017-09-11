ed45b8b90e2fa460724ceae3c09a0152f0f241b6
takka <m@iladdr.es>
2017-09-11 18:25:27 +0900

    MockProgramRepositoryの仕様が間違っていたのを修正

--- Data/Sources/Repository/Mock/MockProgramRepository.swift
+++ Data/Sources/Repository/Mock/MockProgramRepository.swift
@@ -30,11 +30,11 @@ public final class MockProgramRepository: ProgramRepositoryInterface {
                     logoImageURL: nil),
             Program(id: "003",
                     title: "長い番組",
-                    subtitle: "この番組は縦にも横にも長いので、セルの表示が崩れないようにするのが大変だと思いますが頑張ってください。よろしくお願いします。ちなみに画像もあるよ\nすごいですね\n見づらいです",
+                    subtitle: "この番組は縦にも横にも長いので、セルの表示が崩れないようにするのが大変だと思いますが頑張ってください。よろしくお願いします。よく考えたら番組一覧のAPIでは画像が変えることはありませんでした。\nすごいですね\n見づらいです",
                     content: "contentも長いです\n\n\n\n\n\n長いです",
                     startTime: startOfToday + 9.hours + 31.minutes,
                     endTime: startOfToday + 12.hours,
-                    logoImageURL: URL(string: "https://i.imgur.com/jlFgGpe.jpg")),
+                    logoImageURL: nil),
         ]
         
         return Observable<Int>
